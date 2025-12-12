8f482c41f050ddbf55af0cc0d2eb640da72ea32b
HexagramNM <hexagram_n.m@jcom.home.ne.jp>
2025-12-12 23:21:15 +0900

    Fixed folder name for local NAudio.

--- SetupRepository.bat
+++ SetupRepository.bat
@@ -8,19 +8,19 @@ for /f "usebackq delims=" %%A in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\
 
 "%MSBUILD_EXE%" NAudio.sln /m /p:Configuration=Release;Platform="Any CPU"
 
-if not exist "%~dp0LocalNAudio" (
-    mkdir "%~dp0LocalNAudio"
+if not exist "%~dp0NAudioLocal" (
+    mkdir "%~dp0NAudioLocal"
 )
 
-copy "%~dp0external\NAudio\NAudio\bin\Release\NAudio.2.1.0-beta.1.nupkg" "%~dp0LocalNAudio"
-copy "%~dp0external\NAudio\NAudio.Asio\bin\Release\NAudio.Asio.2.1.0-beta.1.nupkg" "%~dp0LocalNAudio"
-copy "%~dp0external\NAudio\NAudio.Core\bin\Release\NAudio.Core.2.1.0-beta.1.nupkg" "%~dp0LocalNAudio"
-copy "%~dp0external\NAudio\NAudio.Extras\bin\Release\NAudio.Extras.2.1.0-beta.1.nupkg" "%~dp0LocalNAudio"
-copy "%~dp0external\NAudio\NAudio.Midi\bin\Release\NAudio.Midi.2.1.0-beta.1.nupkg" "%~dp0LocalNAudio"
-copy "%~dp0external\NAudio\NAudio.Uap\bin\Release\NAudio.Uap.2.1.0-beta.1.nupkg" "%~dp0LocalNAudio"
-copy "%~dp0external\NAudio\NAudio.Wasapi\bin\Release\NAudio.Wasapi.2.1.0-beta.1.nupkg" "%~dp0LocalNAudio"
-copy "%~dp0external\NAudio\NAudio.WinForms\bin\Release\NAudio.WinForms.2.1.0-beta.1.nupkg" "%~dp0LocalNAudio"
-copy "%~dp0external\NAudio\NAudio.WinMM\bin\Release\NAudio.WinMM.2.1.0-beta.1.nupkg" "%~dp0LocalNAudio"
+copy "%~dp0external\NAudio\NAudio\bin\Release\NAudio.2.1.0-beta.1.nupkg" "%~dp0NAudioLocal"
+copy "%~dp0external\NAudio\NAudio.Asio\bin\Release\NAudio.Asio.2.1.0-beta.1.nupkg" "%~dp0NAudioLocal"
+copy "%~dp0external\NAudio\NAudio.Core\bin\Release\NAudio.Core.2.1.0-beta.1.nupkg" "%~dp0NAudioLocal"
+copy "%~dp0external\NAudio\NAudio.Extras\bin\Release\NAudio.Extras.2.1.0-beta.1.nupkg" "%~dp0NAudioLocal"
+copy "%~dp0external\NAudio\NAudio.Midi\bin\Release\NAudio.Midi.2.1.0-beta.1.nupkg" "%~dp0NAudioLocal"
+copy "%~dp0external\NAudio\NAudio.Uap\bin\Release\NAudio.Uap.2.1.0-beta.1.nupkg" "%~dp0NAudioLocal"
+copy "%~dp0external\NAudio\NAudio.Wasapi\bin\Release\NAudio.Wasapi.2.1.0-beta.1.nupkg" "%~dp0NAudioLocal"
+copy "%~dp0external\NAudio\NAudio.WinForms\bin\Release\NAudio.WinForms.2.1.0-beta.1.nupkg" "%~dp0NAudioLocal"
+copy "%~dp0external\NAudio\NAudio.WinMM\bin\Release\NAudio.WinMM.2.1.0-beta.1.nupkg" "%~dp0NAudioLocal"
 
 cd "%~dp0"
 
